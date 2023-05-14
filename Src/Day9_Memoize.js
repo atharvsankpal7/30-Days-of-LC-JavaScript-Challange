5939d167afdcb446abd6fc7adbce9b013f1c2108
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2023-05-14 23:14:22 +0530

    Day9

---
+++ Src/Day9_Memoize.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if (key in cache) {
+            return cache[key];
+        } else {
+            // cache[key]=fn(...args)
+            // return cache
+
+            const retval = fn(...args);
+            cache[key] = retval;
+            return retval;
+        }
+    };
+}
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
