3bfa7b066942d51a4dd63d5fbd2902a007840545
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2023-05-24 23:34:29 +0530

    Day15

---
+++ Day15_Debounce.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+// We want to call the function after given time, but if the function is again called within the execution of first function then we want to prevent the previous call from happening
+var debounce = function (fn, t) {
+    let id;
+    return function (...args) {
+        clearTimeout(id);
+        id = setTimeout(() => {
+            fn(...args);
+        }, t);
+    };
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
